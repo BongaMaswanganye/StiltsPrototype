ce59379299335dd04818c7ff82d048505511bb89
Bonga Maswanganye <snowmehn@gmail.com>
2022-11-17 17:41:28 -0500

    created limits for speed and set up rotation

--- StiltsPrototype/Assets/Scripts/Piloting/DroneMovement.cs
+++ StiltsPrototype/Assets/Scripts/Piloting/DroneMovement.cs
@@ -15,12 +15,14 @@ public class DroneMovement : MonoBehaviour
     public float BaseMoveSpeed = 2;
     public float BaseTurnSpeed = 2;
     public Lever RotationalLever;
-    public Lever ForwardMovementLever;
+    public Lever DirectionalLever;
     public CurrentState DroneState;
+    public float MaxSpeed;
 
-    private float Acceleration;
+    private float DirectionalAcceleration;
+    private float RotationalAcceleration;
     public float AccelerationRate;
-    public int UpperLimit, LowerLimit;
+    public int UpperLeverLimit, LowerLeverLimit;
 
 
     private void Update()
@@ -38,9 +40,9 @@ public class DroneMovement : MonoBehaviour
 
     private void calculateForwardBackwardMovement()
     {
-        Vector3 debugVector = Vector3.forward * ForwardMovementLever.getTilt(); //* Time.deltaTime * BaseMoveSpeed;
-        Drone.transform.position += Vector3.forward * ForwardMovementLever.getTilt(); //* Time.deltaTime * BaseMoveSpeed;
-        Debug.Log("forward backward lever value reads:" + ForwardMovementLever.getTilt() + "drone transform change should add:" + debugVector );
+        Vector3 debugVector = Vector3.forward * DirectionalLever.getTilt(); //* Time.deltaTime * BaseMoveSpeed;
+        Drone.transform.position += Vector3.forward * DirectionalLever.getTilt(); //* Time.deltaTime * BaseMoveSpeed;
+        Debug.Log("forward backward lever value reads:" + DirectionalLever.getTilt() + "drone transform change should add:" + debugVector );
     }
 
     private void calculateRotationalMovement()
@@ -53,24 +55,43 @@ public class DroneMovement : MonoBehaviour
 
     public void FailSafeMovement()
     {
-        switch(ForwardMovementLever.CurrentRead)
+        switch(DirectionalLever.CurrentRead)
         {
             case Lever.LeverState.Forward:
                 Debug.Log("accelerating");
-                Acceleration += AccelerationRate;
+                DirectionalAcceleration += AccelerationRate;
                 break;
             case Lever.LeverState.Backward:
                 Debug.Log("decelerating");
-                Acceleration -= AccelerationRate;
+                DirectionalAcceleration -= AccelerationRate;
                 break;
             case Lever.LeverState.Zero:
-                if (Acceleration > 0) { Acceleration -= AccelerationRate; }
-                if (Acceleration < 0) { Acceleration += AccelerationRate; }
-                if (Acceleration == 0) { Debug.Log("do nothing"); }
+                if (DirectionalAcceleration > 0) { DirectionalAcceleration -= AccelerationRate; }
+                if (DirectionalAcceleration < 0) { DirectionalAcceleration += AccelerationRate; }
+                if (DirectionalAcceleration == 0) { Debug.Log("do nothing"); }
                 break;
         }
-        Acceleration = Mathf.Clamp(Acceleration, LowerLimit, UpperLimit);
-        Drone.transform.position += Vector3.forward * Acceleration * Time.deltaTime * BaseMoveSpeed;
+        DirectionalAcceleration = Mathf.Clamp(DirectionalAcceleration, LowerLeverLimit, UpperLeverLimit);
+        Drone.transform.position += Vector3.forward * DirectionalAcceleration * Time.deltaTime * BaseMoveSpeed;
+
+        switch (RotationalLever.CurrentRead)
+        {
+            case Lever.LeverState.Forward:
+                Debug.Log("accelerating");
+                RotationalAcceleration += AccelerationRate;
+                break;
+            case Lever.LeverState.Backward:
+                Debug.Log("decelerating");
+                RotationalAcceleration -= AccelerationRate;
+                break;
+            case Lever.LeverState.Zero:
+                if (RotationalAcceleration > 0) { RotationalAcceleration -= AccelerationRate; }
+                if (RotationalAcceleration < 0) { RotationalAcceleration += AccelerationRate; }
+                if (RotationalAcceleration == 0) { Debug.Log("do nothing"); }
+                break;
+        }
+        RotationalAcceleration = Mathf.Clamp(RotationalAcceleration, LowerLeverLimit, UpperLeverLimit);
+        Drone.transform.Rotate(Vector3.forward * Mathf.Clamp(DirectionalAcceleration * Time.deltaTime * BaseMoveSpeed,-1*MaxSpeed,MaxSpeed));
 
     }
 
